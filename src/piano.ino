a999f73cd1dd5988e625f51683d3aa952f68c751
weaselsanddragon <43973195+weaselsanddragon@users.noreply.github.com>
2018-11-26 22:12:17 -0600

    sharp and flat functionality
    added basic code to play one tone higher or lower based on a photoresistor

--- src/piano.ino
+++ src/piano.ino
@@ -11,8 +11,12 @@ Modified by Desmond Ku
 #include <CapacitiveSensor.h>
 
 
- 
+// pin definition
 #define speaker 11
+#define light 5
+
+// value of photoresistor
+int value;
 
 
 // Set the Send Pin & Receive Pin.
@@ -49,11 +53,14 @@ void loop()
   long total7 =  cs_2_9.capacitiveSensor(3000);
   long total8 =  cs_2_10.capacitiveSensor(3000);
   
-
+  value = analogRead(light);
 
   Serial.print(millis() - start);        // check on performance in milliseconds
   Serial.print("\t");                    // tab character for debug windown spacing
 
+  Serial.print(value);                   // print value of photoresistor
+  Serial.print("\t");                    // Leave some space before print the next output
+ 
   Serial.print(total1);                  // print sensor output 1
   Serial.print("\t");                    // Leave some space before print the next output
   Serial.print(total2);                  // print sensor output 2
@@ -74,18 +81,41 @@ void loop()
   
   
   
-  
-  // When hand is touched the sensor, the speaker will produce a tone.
-  // I set a threshold for it, so that the sensor won't be too sensitive.
-  // changed original 500 to 1000 for better testing
-  if (total1 > 1000) tone(speaker,131);   // frequency
-  if (total2 > 1000) tone(speaker,147);   // you can see https://www.arduino.cc/en/Tutorial/toneMelody if you want to change frequency
-  if (total3 > 1000) tone(speaker,165);
-  if (total4 > 1000) tone(speaker,175);
-  if (total5 > 1000) tone(speaker,196);
-  if (total6 > 1000) tone(speaker,220);
-  if (total7 > 1000) tone(speaker,247);
-  if (total8 > 1000) tone(speaker,262);
+  if (value < 50) // flat tone
+  {
+    // When hand is touched the sensor, the speaker will produce a tone.
+    // I set a threshold for it, so that the sensor won't be too sensitive.
+    if (total1 > 1000) tone(speaker,123);   // frequency
+    if (total2 > 1000) tone(speaker,139);   // you can see https://www.arduino.cc/en/Tutorial/toneMelody if you want to change frequency
+    if (total3 > 1000) tone(speaker,156);
+    if (total4 > 1000) tone(speaker,165);
+    if (total5 > 1000) tone(speaker,185);
+    if (total6 > 1000) tone(speaker,208);
+    if (total7 > 1000) tone(speaker,233);
+    if (total8 > 1000) tone(speaker,247);
+  }
+  else if (value > 700) // sharp tone
+  {
+    if (total1 > 1000) tone(speaker,139);   // frequency
+    if (total2 > 1000) tone(speaker,156);   // you can see https://www.arduino.cc/en/Tutorial/toneMelody if you want to change frequency
+    if (total3 > 1000) tone(speaker,175);
+    if (total4 > 1000) tone(speaker,185);
+    if (total5 > 1000) tone(speaker,208);
+    if (total6 > 1000) tone(speaker,233);
+    if (total7 > 1000) tone(speaker,262);
+    if (total8 > 1000) tone(speaker,277);
+  }
+  else // base tone (white keys)
+  {
+    if (total1 > 1000) tone(speaker,131);   // frequency
+    if (total2 > 1000) tone(speaker,147);   // you can see https://www.arduino.cc/en/Tutorial/toneMelody if you want to change frequency
+    if (total3 > 1000) tone(speaker,165);
+    if (total4 > 1000) tone(speaker,175);
+    if (total5 > 1000) tone(speaker,196);
+    if (total6 > 1000) tone(speaker,220);
+    if (total7 > 1000) tone(speaker,247);
+    if (total8 > 1000) tone(speaker,262);
+  }
   
   // When hand didn't touch on it, no tone is produced.
   // because of change above change 500 to 1000
